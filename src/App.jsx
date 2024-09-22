3420a55697870cf1482335a1a2f7421f83e4985b
ajay2512 <bhakarajay033@gmail.com>
2024-09-23 01:02:22 +0530

    videos data added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
 import Video from "./components/Video";
-
+import data from "./data/data";
 function App() {
+  
   return (
     <>
-      <Video
-        title="video1"
-        channel="reactjs"
-        views="20K"
-        time="20 years ago "
-        verified={true}
-      />
-      <Video
-        title="video2"
-        channel="nodejs"
-        views="100K"
-        time="10 years ago"
-        verified={false}
-      />
+      <div>Videos</div>
+      {data.map((video, index) => (
+        <Video
+          key={index}  
+          title={video.title}  
+          channel={video.channel}
+          views={video.views}
+          time={video.time}
+          verified={video.verified}
+          id={video.id}
+        />
+      ))}
     </>
   );
 }
---
+++ src/data/data.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    title: "video1",
+    channel: "nodejs",
+    views: "100K",
+    time: "9 years ago",
+    verified: true,
+  },
+  {
+    id: 2,
+    title: "video2",
+    channel: "reactjs",
+    views: "100K",
+    time: "10 years ago",
+    verified: false,
+  },
+  {
+    id: 3,
+    title: "video3",
+    channel: "angularjs",
+    views: "1M",
+    time: "11 years ago",
+    verified: false,
+  },
+];
